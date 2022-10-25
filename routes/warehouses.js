f9d0eec9754a64afcd84a73e9d4ccda98b0d3b98
irjala <jonasirjala@gmail.com>
2022-10-25 14:15:41 +0300

    warehouse router

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const router = express.Router()
-
 const Warehouse = require('../models/warehouseModel')
+const { updateOne } = require('../models/warehouseModel')
+
 
 // GET all warehouses
 
+router.get('/', async (req, res) => {
+    try {
+        const warehouses = await Warehouse.find()
+        res.send(warehouses)
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
+})
 
 // POST a new warehouse
-router.post('/', async (req, res) => {
+router.post('/', authToken, async (req, res) => {
     try {
         const warehouse = new Warehouse({
             city: req.body.city,
@@ -27,5 +36,34 @@ router.post('/', async (req, res) => {
 
 // PATCH
 
+router.patch('/:id', authToken, async (req,res) =>{
+    try {
+        const updateWH = await Warehouse.findByIdAndUpdate(
+            {_id: req.params.id, createdBy: req.authUser.sub}, // Kanske måste ändra på createdBy
+            req.body, // Updateringen som skall ske
+            {new: true})
+        res.send({msg: "Warehouse has been updated", updateWH: updateOne})
+    } catch (error) {
+        res.status(500).send({msg: error.message})
+    }
+})
+
+
 // DELETE
 
+router.delete('/:id', authToken, async (req,res) => {
+    try {
+        const deleteWarehouse = await Cabin.deleteOne({
+            _id: req.params.id,
+            createdBy: req.authUser.sub })
+        if(!deleteWarehouse){
+            return res.status(404).send({msg: "No such warehouse"})
+        }
+        res.send(deleteWarehouse)
+    } catch (error) {
+        res.status(500).send({msg: error.message})
+    }
+})
+
+
+module.exports = router
