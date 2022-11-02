547d71ef08c75252acd1c0f5b6c7c6575ce9c024
irjala <jonasirjala@gmail.com>
2022-11-02 13:28:02 +0200

    product model update

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,3 @@
-
 /*
 namn,
 {
@@ -14,3 +13,30 @@ namn,
     amount
 }
 */
+
+const warehouseSchema = new mongoose.Schema({
+    warehouse: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Int32,
+        required: true
+    }
+}, { timestamps: true });
+
+const mongoose = require('mongoose')
+
+const inventorySchema = new mongoose.Schema({
+    product: {
+        type: String,
+        required: true
+    },
+    wareHouse: [warehouseSchema]
+}, { timestamps: true });
+
+
+
+
+
+module.exports = mongoose.model('Inventory', inventorySchema)
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -6,7 +6,7 @@ const { updateOne } = require('../models/warehouseModel')
 
 // GET all warehouses
 
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
     try {
         const warehouses = await Warehouse.find()
         res.send(warehouses)
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
 })
 
 // POST a new warehouse
-router.post('/', authToken, async (req, res) => {
+router.post('/', authToken, async(req, res) => {
     try {
         const warehouse = new Warehouse({
             city: req.body.city,
@@ -36,32 +36,32 @@ router.post('/', authToken, async (req, res) => {
 
 // PATCH
 
-router.patch('/:id', authToken, async (req,res) =>{
+router.patch('/:id', authToken, async(req, res) => {
     try {
-        const updateWH = await Warehouse.findByIdAndUpdate(
-            {_id: req.params.id, createdBy: req.authUser.sub}, // Kanske måste ändra på createdBy
+        const updateWH = await Warehouse.findByIdAndUpdate({ _id: req.params.id, createdBy: req.authUser.sub }, // Kanske måste ändra på createdBy
             req.body, // Updateringen som skall ske
-            {new: true})
-        res.send({msg: "Warehouse has been updated", updateWH: updateOne})
+            { new: true })
+        res.send({ msg: "Warehouse has been updated", updateWH: updateOne })
     } catch (error) {
-        res.status(500).send({msg: error.message})
+        res.status(500).send({ msg: error.message })
     }
 })
 
 
 // DELETE
 
-router.delete('/:id', authToken, async (req,res) => {
+router.delete('/:id', authToken, async(req, res) => {
     try {
-        const deleteWarehouse = await Cabin.deleteOne({
+        const deleteWarehouse = await Warehouse.deleteOne({
             _id: req.params.id,
-            createdBy: req.authUser.sub })
-        if(!deleteWarehouse){
-            return res.status(404).send({msg: "No such warehouse"})
+            createdBy: req.authUser.sub
+        })
+        if (!deleteWarehouse) {
+            return res.status(404).send({ msg: "No such warehouse" })
         }
         res.send(deleteWarehouse)
     } catch (error) {
-        res.status(500).send({msg: error.message})
+        res.status(500).send({ msg: error.message })
     }
 })
 
