89a58f97a93ec2f36bddb239cd808c192cb9d596
Isak Svarvar <isak.svarvar@gmail.com>
2022-11-08 14:12:59 +0200

    warehouseCheck middleware update

--- middleware/warehouseCheck.js
+++ middleware/warehouseCheck.js
@@ -1,3 +1,12 @@
 module.exports = (req, res, next) => {
-    //in progress
+    try {
+        //const warehouse = req.body['warehouse']
+        //in progress, need to fix products POST structure so I got corret check structure
+        
+    } catch (error) {
+        return res.status(404).send({
+            msg: "No such warehouse found",
+            error: error.message
+        })
+    }
 }
