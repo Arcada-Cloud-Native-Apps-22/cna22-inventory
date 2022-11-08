d0062871f560fb09c83060203d374132e3db849a
Isak Svarvar <isak.svarvar@gmail.com>
2022-11-08 16:40:47 +0200

    comments, made warehouseModel name unique

--- middleware/warehouseCheck.js
+++ middleware/warehouseCheck.js
@@ -3,22 +3,23 @@ const Warehouse = require('../models/warehouseModel')
 module.exports = async (req, res, next) => {
 
     try {
+        //pulls info from body.warehouses
         const warehouses = req.body.warehouses
-        //loop through
+        //loop through warehouses
         for (let i = 0; i < warehouses.length; i++){
             console.log(warehouses[i])
             const warehouse = await Warehouse.find({name: warehouses[i].name})
             if(warehouse.length){
+                //found warehouse with matching name
                 console.log("found warehouse: " + warehouses[i].name + " under: " + warehouse)
             } else{
+                //no warehouse matching name
                 console.log("bogus warehouse detected!: " + warehouses[i])
                 console.log("aborting...")
                 return res.status(404).send({msg: "Warehouse not found: " + warehouses[i].name})
             }
         }
-        //const warehouse = req.body['warehouse']
-        //in progress, need to fix products POST structure so I got corret check structure
-        
+    
     } catch (error) {
         return res.status(500).send({msg: error.message})
     }
